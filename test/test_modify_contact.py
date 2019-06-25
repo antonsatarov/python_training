9ae611d7b404919fe0ef5d521d52e61be1eeb536
asatarov <anton.satarov@gmail.com>
2019-06-25 11:13:40 +0700

    finish comparison checks for contact (task 11)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,15 +5,20 @@ def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
     old_contact_list = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="New first name"))
+    contact = Contact(firstname="New first name")
+    contact.id = old_contact_list[0].id
+    contact.lastname = old_contact_list[0].lastname
+    app.contact.modify_first_contact(contact)
     new_contact_list = app.contact.get_contact_list()
     assert len(old_contact_list) == len(new_contact_list)
+    old_contact_list[0] = contact
+    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
 
 
-def test_modify_contact_bday(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(bday="1"))
-    old_contact_list = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(bday="2"))
-    new_contact_list = app.contact.get_contact_list()
-    assert len(old_contact_list) == len(new_contact_list)
+#def test_modify_contact_bday(app):
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(bday="1"))
+#    old_contact_list = app.contact.get_contact_list()
+#    app.contact.modify_first_contact(Contact(bday="2"))
+#    new_contact_list = app.contact.get_contact_list()
+#    assert len(old_contact_list) == len(new_contact_list)
